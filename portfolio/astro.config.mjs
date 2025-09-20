b31ca7085ff99d498ea1493d4849dc8df518990b
Manuel Blanco <manuel.blanco.1995@gmail.com>
2025-09-20 19:56:44 -0300

    ver6

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  base: '/portfolio/',  // Reemplaza con el nombre de tu repo
-  output: 'static',          // Para que genere archivos estáticos
+  base: '/portfolio/'      
 });
