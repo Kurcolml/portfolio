5c4eb4399dee3a54ff20cba8da7cf26b41a48acf
Manuel Blanco <manuel.blanco.1995@gmail.com>
2025-09-20 20:26:38 -0300

    Update astro.config.mjs

--- portfolio/astro.config.mjs
+++ portfolio/astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://kurcolml.github.io',
+  site: 'https://kurcolml.github.io', 
+  
   base: '/portfolio/'      
 });
